0051d191bece707d1e22694120ea59b2fcaf97c8
Erik Hofer <erik-hofer@mail.de>
2023-04-12 13:16:39 +0200

    Add leaderboard

---
+++ src/leaderboard/Leaderboard.tsx
@@ -0,0 +1,96 @@
+import { FrameHexagon, Table } from "@arwes/core"
+import { useQuery } from "react-query"
+import { useAutoScroll } from "./useAutoScroll"
+
+export interface Challenge {
+  id: number
+  name: string
+  difficulty: number
+}
+
+export interface Team {
+  id: number
+  name: string
+  solvedChallengeIds: number[]
+}
+
+export interface LeaderboardData {
+  challenges: Challenge[]
+  teams: Team[]
+}
+
+export const Leaderboard: React.FC = () => {
+  const { isLoading, error, data } = useQuery<LeaderboardData>(
+    'leaderboardData',
+    () => fetch('https://hacking-night-backend.netlify.app/.netlify/functions/leaderboard').then(res =>res.json()),
+    { refetchInterval: 5000 }
+  )
+
+  useAutoScroll()
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+  if (error || !data) {
+    return <span>Error: {"" + error}</span>
+  }
+  if (data.teams.length === 0) {
+    return <span>Nicht aktiv, warte auf die nächste Hacking Night!</span>
+  } 
+
+  const columnWidths = ["20%", ...data.teams.map((t) => `${80 / data.teams.length}%`)]
+
+  const headers = [
+    { id: "challenge", data: "Challenge" },
+    ...data.teams.map((team) => ({ id: team.id, data: team.name })),
+  ]
+
+  const dataset = data.challenges.map((challenge) => ({
+    id: challenge.id,
+    columns: [
+      { id: "challenge", data: <ChallengeComponent challenge={challenge} key={challenge.id} /> },
+      ...data.teams.map((team) => ({
+        id: team.id,
+        data: (
+          <SolvedComponent solved={team.solvedChallengeIds.includes(challenge.id)} key={team.id} />
+        ),
+      })),
+    ],
+  }))
+
+  return (
+    <div>
+      <Table
+        headers={headers}
+        dataset={dataset}
+        columnWidths={columnWidths}
+        animator={{ animate: false /* With animation, updating the content doesn#t work... */ }}
+      />
+    </div>
+  )
+}
+
+const ChallengeComponent: React.FC<{ challenge: Challenge }> = (props) => {
+  const { difficulty, name } = props.challenge
+  return (
+    <div style={{ display: "flex", height: 42, alignItems: "center", gap: 16 }}>
+      <div style={{ width: 70, textAlign: "center" }}>
+        {new Array(difficulty).fill(0).map((_, i) => (
+          <span key={i}>
+            ⭐{(difficulty === 4 && i === 1) || (difficulty > 4 && i == 2) ? <br /> : null}
+          </span>
+        ))}
+      </div>
+      <div style={{ fontSize: 16 }}>{name}</div>
+    </div>
+  )
+}
+
+const SolvedComponent: React.FC<{ solved: boolean }> = ({ solved }) => {
+  return solved ? (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      {/* @ts-ignore */}
+      <FrameHexagon palette="success">✔</FrameHexagon>
+    </div>
+  ) : null
+}
---
+++ src/leaderboard/useAutoScroll.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { useHotkeys as usHK } from 'react-hotkeys-hook'
+
+export function useAutoScroll() {
+  const [active, setActive] = useState(false)
+
+  usHK('ctrl+k', (e) => {
+    e.preventDefault()
+    setActive(!active)
+  }, [active])
+
+  useEffect(() => {
+    if (!active) {
+      return
+    }
+    const max = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    )
+    let curr = 0
+    let dir = 1
+    const interval = setInterval(() => {
+      if (curr >= max) {
+        dir = -1
+      } else if (curr <= 0) {
+        dir = 1
+      }
+      window.scrollTo({ top: curr, behavior: "smooth" })
+      curr += dir * 1
+    }, 100)
+    return () => {
+      console.log("CLEAR INTERVAL")
+      clearInterval(interval)
+    }
+  }, [active])
+}
+
+function useHotkeys(arg0: string, arg1: () => any, arg2: any[]) {
+  throw new Error("Function not implemented.")
+}
